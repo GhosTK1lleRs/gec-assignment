2776910e2ded21389256358101a8c3df33a662aa
GhosTK1lleRs <aghostkiller@gmail.com>
2020-07-11 20:39:35 +0700

    Update head.blade.php

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -16,5 +16,4 @@
 
     <!-- font awesome -->
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-    {{-- <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet"> --}}
 </head>
